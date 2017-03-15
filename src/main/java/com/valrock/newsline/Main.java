ef72591ccbf3d3617f0040a24c8cfd69f0329e75
Валерий <valeriinalimov@mail.ru>
2017-03-15 18:22:34 +0700

    Initial commit

---
+++ src/main/java/com/valrock/newsline/Main.java
@@ -0,0 +1,10 @@
+package com.valrock.newsline;
+
+/**
+ * Created by Валерий on 15.03.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello test task");
+    }
+}
