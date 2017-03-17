5a6d7d46bd2d30f5e1c89c515af02fb017d95ff8
Валерий <valeriinalimov@mail.ru>
2017-03-17 11:25:01 +0700

    add logback

--- src/main/java/com/valrock/newsline/web/NewsServlet.java
+++ src/main/java/com/valrock/newsline/web/NewsServlet.java
@@ -1,17 +1,24 @@
 package com.valrock.newsline.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 /**
  * Created by Валерий on 16.03.2017.
  */
 public class NewsServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(NewsServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/news.jsp").forward(request, response);
+        LOG.debug("redirect to news");
+//        request.getRequestDispatcher("/news.jsp").forward(request, response);
+        response.sendRedirect("news.jsp");
     }
 }
