0d0e8a2e57c40f0f6340bda6da70ba1a2218a97f
Валерий <valeriinalimov@mail.ru>
2017-03-19 19:30:04 +0700

    profiles connection pool

---
+++ src/main/java/com/valrock/newsline/Profiles.java
@@ -0,0 +1,14 @@
+package com.valrock.newsline;
+
+/**
+ * Created by Валерий on 19.03.2017.
+ */
+public class Profiles {
+    public static final String
+            POSTGRES = "postgres",
+            HSQLDB = "hsqldb",
+            JDBC = "jdbc",
+            JPA = "jpa";
+
+    public static final String ACTIVE_DB = POSTGRES;
+}
