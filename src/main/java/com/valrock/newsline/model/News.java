420c01ec592fe51406be97eb7170dc2bd021b201
Валерий <valeriinalimov@mail.ru>
2017-03-19 19:15:19 +0700

    coding JpaNewsRepositoryImpl

--- src/main/java/com/valrock/newsline/model/News.java
+++ src/main/java/com/valrock/newsline/model/News.java
@@ -1,7 +1,9 @@
 package com.valrock.newsline.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -9,17 +11,43 @@ import java.time.LocalTime;
 /**
  * Created by Валерий on 16.03.2017.
  */
+@SuppressWarnings("JpaQlInspection")
+@NamedQueries({
+        @NamedQuery(name = News.ALL_SORTED,
+        query = "SELECT n FROM News n WHERE n.user.id=:userId ORDER BY n.dateTime DESC"),
+        @NamedQuery(name = News.DELETE,
+        query = "DELETE FROM News n WHERE n.id=:id AND n.user.id=:userId"),
+        @NamedQuery(name = News.GET_BETWEEN,
+        query = "SELECT n FROM News n WHERE n.user.id=:userId AND n.dateTime BETWEEN :startDateTime AND :endDateTime ORDER BY n.dateTime DESC")
+})
+@Entity
+@Table(name = "news", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"userId", "dateTime"}, name = "news_unique_user_datetime_ind")
+})
 public class News extends BaseEntity{
 
+    public static final String ALL_SORTED = "News.getAll";
+    public static final String DELETE = "News.delete";
+    public static final String GET_BETWEEN = "News.getBetween";
+
+    @Column(name = "newsHeader", nullable = false)
+    @NotBlank
     private String newsHeader;
 
+    @Column(name = "dateTime", nullable = false)
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name = "textnews", nullable = false)
+    @NotBlank
     private String textnews;
 
+    @Column(name = "imageName", nullable = false)
+    @NotBlank
     private String imageName;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
     private User user;
 
     public News(){}
--- src/main/java/com/valrock/newsline/repository/jpa/JpaNewsRepositoryImpl.java
+++ src/main/java/com/valrock/newsline/repository/jpa/JpaNewsRepositoryImpl.java
@@ -1,41 +1,77 @@
 package com.valrock.newsline.repository.jpa;
 
 import com.valrock.newsline.model.News;
+import com.valrock.newsline.model.User;
 import com.valrock.newsline.repository.NewsRepository;
 import org.apache.commons.fileupload.FileItem;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.Collection;
+
+import static com.valrock.newsline.util.NewsUtil.deleteFile;
+import static com.valrock.newsline.util.NewsUtil.saveFile;
 
 /**
  * Created by Валерий on 19.03.2017.
  */
 @Repository
+@Transactional(readOnly = true)
 public class JpaNewsRepositoryImpl implements NewsRepository{
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
+    @Transactional
     public News save(News news, int userId, String path, FileItem item) {
-        return null;
+        if (!news.isNew() && get(news.getId(), userId) == null){
+            return null;
+        }
+        news.setUser(em.getReference(User.class, userId));
+        if (news.isNew()){
+            news.setImageName(saveFile(path, item));
+            em.persist(news);
+            return news;
+        } else {
+            deleteFile(path + get(news.getId(), userId).getImageName());
+            news.setImageName(saveFile(path, item));
+            return em.merge(news);
+        }
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId, String path) {
-        return false;
+        deleteFile(path + get(id, userId).getImageName());
+        return em.createNamedQuery(News.DELETE)
+                .setParameter("id", id)
+                .setParameter("userId", userId)
+                .executeUpdate() != 0;
     }
 
     @Override
     public News get(int id, int userId) {
-        return null;
+        News news = em.find(News.class, id);
+        return news != null && news.getUser().getId() == userId ? news : null;
     }
 
     @Override
     public Collection<News> getAll(int userId) {
-        return null;
+        return em.createNamedQuery(News.ALL_SORTED, News.class)
+                .setParameter("userId", userId)
+                .getResultList();
     }
 
     @Override
     public Collection<News> getBetween(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return em.createNamedQuery(News.GET_BETWEEN, News.class)
+                .setParameter("userId", userId)
+                .setParameter("startDateTime", startDateTime)
+                .setParameter("endDateTime", endDateTime)
+                .getResultList();
     }
 }
